96135150e79c74f08a6e9b5d1b40b6ba692c32af
meetqy <meetqy.com@gmail.com>
2018-02-26 14:12:47 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,23 @@ var gulp = require("gulp"),
     del = require('del');
 
 gulp.task('css',['clear_css'],function () {
-   gulp.src('assets/css/*.css')
-       .pipe(concat('caffeine-theme.min.css'))
-       .pipe(cssmin({
-           advanced: true,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
-           compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
-           keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
-           keepSpecialComments: '*'
-       }))
-       .pipe(gulp.dest('./assets/css/'));
+    gulp.src('assets/css/*.css')
+        .pipe(concat('caffeine-theme.min.css'))
+        .pipe(cssmin({
+            advanced: true,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*'
+        }))
+        .pipe(gulp.dest('./assets/css/'));
 });
 
 var js = 'assets/js/src';
 gulp.task('js',['clear_js'],function () {
-   gulp.src(['assets/js/caffeine-theme.js',js+'/other.js'])
-       .pipe(concat('caffeine-theme.min.js'))
-       .pipe(uglify())
-       .pipe(gulp.dest('assets/js/'))
+    gulp.src(['assets/js/caffeine-theme.js',js+'/other.js'])
+        .pipe(concat('caffeine-theme.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js/'))
 });
 
 gulp.task('clear_css', function(cb) {
