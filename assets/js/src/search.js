ca9b21a0ca7d1fb4e8cfc9be31ae919ae9511a10
meetqy <meetqy.com@gmail.com>
2018-02-26 14:13:45 +0800

    Revert "鎼滅礌浼樺寲"
    This reverts commit cbb0ee29d7c491a8c6ea085d0230752e3871779b.
    
    # Conflicts:
    #	assets/js/caffeine-theme.js
    #	assets/js/caffeine-theme.min.js
    #	assets/js/src/search.js

--- assets/js/src/search.js
+++ assets/js/src/search.js
@@ -19,7 +19,11 @@ $(function() {
         zeroResultsInfo: false,
         onKeyUp: true,
         displaySearchInfo: true,
+<<<<<<< HEAD
         result_template: "<a class=\"result\" href='{{link}}'>\n  <h2>{{title}}</h2>\n  <h4><time datetime=\"{{date published_at format=\"YYYY-MM-DD\"}}\">{{date published_at format=\"YYYY-MM-DD\"}}</time></h4>\n</a>",
+=======
+        result_template: "<a class=\"result\" href='{{link}}'>\n  <h2>{{title}}</h2>\n  <h4>{{pubDate}}</h4>\n</a>",
+>>>>>>> parent of cbb0ee2... 搜素优化
         onComplete: function(query) {
             if (query.length > 0) {
                 return hideTags();
