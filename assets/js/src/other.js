ca9321c53717e716ebe214cc7ed7a7874c91d168
meetqy <meetqy.com@gmail.com>
2018-02-27 17:07:49 +0800

    根据tag获取文章列表

--- assets/js/src/other.js
+++ assets/js/src/other.js
@@ -1,4 +1,27 @@
-$('.works-show-tags a').on('click',function () {
-    $('.works-show-tags a').removeClass('works-show-active');
-    $(this).addClass('works-show-active');
-})
+(function($){
+	// 作品展示
+	function page_works_show() {
+		if(window.location.pathname != '/works-show/') return false;
+		var cur_page = $('.works-show-active')[0].className.split(' ')[0];
+		get_data();
+		$('.works-show-tags a').on('click',function () {
+		    $('.works-show-tags a').removeClass('works-show-active');
+		    $(this).addClass('works-show-active');
+		    cur_page = this.className.split(' ')[0];
+		    get_data();
+		});
+
+		// 获取对应tag文章
+		function get_data() {
+			$.get(ghost.url.api('posts', {filter:'tags:'+cur_page+''})).done(function (data){
+			  	var posts = data.posts;
+			  	console.log(posts);
+			}).fail(function (err){
+			  console.log(err);
+			});
+		}
+	}
+	
+	page_works_show();
+})($)
+
